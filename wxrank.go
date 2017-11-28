06149548ac2321371112a27c5479289b5c49d75d
Administrator <245561237@qq.com>
2017-11-28 13:55:37 +0800

    ss

--- wxrank.go
+++ wxrank.go
@@ -7,7 +7,7 @@ func main() {
 	e.File("favicon.ico", "images/favicon.ico")
 	e.File("logo.png", "images/80x80logo.png")
 	e.File("/", "static/dist/index.html")
-	e.File("/cate/:id", "static/dist/index.html")
+	e.File("/t/:id", "static/dist/index.html")
 	e.File("/tags", "static/dist/index.html")
 	e.File("/hot", "static/dist/index.html")
 	e.File("/new", "static/dist/index.html")
