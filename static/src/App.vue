a35bbc489f60e30e59f99a5ea8658ac7e8cbc1d0
Administrator <245561237@qq.com>
2017-11-29 12:37:18 +0800

    test

--- static/src/App.vue
+++ static/src/App.vue
@@ -54,8 +54,8 @@ import api from './api';
 
  api.get("/jssdk?url=http://readfollow.com/hot",function(err,data){
     wx.config({
-      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: "wx702b93aef72f3549", // 必填，公众号的唯一标识
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: "wx267866e82ab809fc", // 必填，公众号的唯一标识
       timestamp: data.timestamp, // 必填，生成签名的时间戳
       nonceStr: data.nonceStr, // 必填，生成签名的随机串
       signature: data.signature, // 必填，签名，见附录1
