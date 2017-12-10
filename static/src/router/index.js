c5cb4feb6bba6b83f80d45d17eb9d6f37b57fe95
Administrator <245561237@qq.com>
2017-12-10 16:22:02 +0800

    搜索功能

--- static/src/router/index.js
+++ static/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import homePage from '@/views/Home'
 import hotPage from '@/views/Hot'
+import searchPage from '@/views/Search'
 import CatePage from '@/views/Cate' // 进入某个分类
 // import HelloWorld from '@/components/HelloWorld'
 // import mediaPage from '@/views/Media'
@@ -9,7 +10,7 @@ import CatePage from '@/views/Cate' // 进入某个分类
 // import aboutPage from '@/views/about'
 // import guidePage from '@/views/guide'
 // import postPage from '@/views/Post'
-// import TagPage from '@/views/Tag' // 展示所有标签
+import TagPage from '@/views/Tag' // 展示所有标签
 
 // // 反馈模块
 // import fbArticlePage from '@/pages/feedback/Article'
@@ -50,7 +51,13 @@ export default new Router({
             path:'/t/:id', 
             name: 'cate',
             component:hotPage
-        },      
+        },
+        { 
+            path: '/search',
+            name: 'search',
+            component: searchPage,
+            props: (route) => ({ query: route.query.q }) 
+        },
         // {
         //     path:'/swiper',
         //     name: 'swiper',
@@ -64,11 +71,11 @@ export default new Router({
         //     path:'/post',
         //     component:postPage
         // },
-        // {
-        //     path:'/tags', 
-        //     name: 'tags',
-        //     component:TagPage
-        // },
+        {
+            path:'/tags', 
+            name: 'tags',
+            component:TagPage
+        },
         // {
         //     path:'/p/:id',
         //     name: 'media',
--- static/src/views/Hot.vue
+++ static/src/views/Hot.vue
@@ -17,7 +17,7 @@
 
     </label>
   </div> -->
-
+<Search></Search>
   
   <ArticleList :articles="articles"></ArticleList>
 
@@ -75,7 +75,8 @@
 </style>
 <script>
 
-import ArticleList from '@/components/ArticleList';
+import ArticleList from '@/components/ArticleList'
+import Search from '@/components/Search';
 import InfiniteLoading from 'vue-infinite-loading'
 import api from '../api';
 
@@ -85,6 +86,7 @@ export default {
   
   
     components: {
+      Search,
       InfiniteLoading,
       ArticleList
     },
