558be0d921a6facf2ee635bdb711fed6b628f731
Administrator <245561237@qq.com>
2017-12-15 15:33:20 +0800

    2 https

--- static/config/prod.env.js
+++ static/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://api.readfollow.com"',
-  BASE_URL: '"http://readfollow.com"'
+  API_ROOT: '"https://api.readfollow.com"',
+  BASE_URL: '"https://readfollow.com"'
 }
