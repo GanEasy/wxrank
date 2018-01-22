543007b25209e22987ef9943bea3b5a183f07d76
Administrator <245561237@qq.com>
2018-01-22 16:21:40 +0800

    图片懒加载

--- static/src/components/ArticleList.vue
+++ static/src/components/ArticleList.vue
@@ -8,8 +8,8 @@
       <div  v-for="article in articles" :key="article.id" class="weui-media-box weui-media-box_appmsg" >
 
           <div class="weui-media-box__hd" style="width:80px">
-            <!-- <a :href="article.URL"><img class="weui-media-box__thumb"   v-lazy="article.Cover" ></a> -->
-            <a :href="article.URL"><img class="weui-media-box__thumb"   :src="article.Cover" ></a>
+            <a :href="article.URL"><img class="weui-media-box__thumb"   v-lazy="article.Cover" ></a>
+            <!-- <a :href="article.URL"><img class="weui-media-box__thumb"   :src="article.Cover" ></a> -->
           </div>  
           <div style="width:30px">
             <ul class="weui-media-box__info article-tags">
