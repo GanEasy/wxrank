97780421af6833cc9919d76e223cbb86e3fd7832
Administrator <245561237@qq.com>
2018-01-22 16:18:17 +0800

    不要 jssdk

--- static/src/main.js
+++ static/src/main.js
@@ -99,34 +99,34 @@ Vue.component(VmBackTop.name, VmBackTop)
 			window.ga('set', 'page', to.fullPath);
 			window.ga('send', 'pageview');
 		  }
-        setTimeout(function(){
-            api.get("/jssdk?url=http://readfollow.com"+to.path,function(err,data){
-                wx.config({
-                  debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                  appId: "wx267866e82ab809fc", // 必填，公众号的唯一标识
-                  timestamp: data.timestamp, // 必填，生成签名的时间戳
-                  nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                  signature: data.signature, // 必填，签名，见附录1
-                  jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                })
+        // setTimeout(function(){
+        //     api.get("/jssdk?url=http://readfollow.com"+to.path,function(err,data){
+        //         wx.config({
+        //           debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        //           appId: "wx267866e82ab809fc", // 必填，公众号的唯一标识
+        //           timestamp: data.timestamp, // 必填，生成签名的时间戳
+        //           nonceStr: data.nonceStr, // 必填，生成签名的随机串
+        //           signature: data.signature, // 必填，签名，见附录1
+        //           jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        //         })
             
                 
-                wx.ready(function () {
-                    wx.checkJsApi({jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage']});
-                    wx.onMenuShareTimeline({
-                        title: '跟读，优质微信文章聚合平台',
-                        link: "http://readfollow.com"+to.path,
-                        imgUrl: 'http://readfollow.com/logo.png'
-                    });
-                    wx.onMenuShareAppMessage({
-                        title: '跟读，微信优质文章聚合平台',
-                        desc: '阅读优质的微信文章，节省翻看订阅号的时间。',
-                        link: "http://readfollow.com"+to.path,
-                        imgUrl: 'http://readfollow.com/logo.png'
-                    })
-                })
-              })
-        }, 200);
+        //         wx.ready(function () {
+        //             wx.checkJsApi({jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage']});
+        //             wx.onMenuShareTimeline({
+        //                 title: '跟读，优质微信文章聚合平台',
+        //                 link: "http://readfollow.com"+to.path,
+        //                 imgUrl: 'http://readfollow.com/logo.png'
+        //             });
+        //             wx.onMenuShareAppMessage({
+        //                 title: '跟读，微信优质文章聚合平台',
+        //                 desc: '阅读优质的微信文章，节省翻看订阅号的时间。',
+        //                 link: "http://readfollow.com"+to.path,
+        //                 imgUrl: 'http://readfollow.com/logo.png'
+        //             })
+        //         })
+        //       })
+        // }, 200);
       })
 /* eslint-disable no-new */
 new Vue({
